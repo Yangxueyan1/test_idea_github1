7c4b5daa425b02d3b68aa1187a9afbc31e0b922a
verna <895380131@qq.com>
2021-10-02 12:38:43 +0800

    老师修复git push

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
         System.out.println("1212");
         System.out.println("测试");
         System.out.println("重新配置测试");
+        System.out.println("push");
+
     }
 }
