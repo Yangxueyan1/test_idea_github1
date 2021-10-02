2edaa96e6e7303fd57d0893e548c7c7aefbeafe9
verna <895380131@qq.com>
2021-10-02 12:19:45 +0800

    重新配置命令行测试

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
         System.out.println("HelloWorld....");
         System.out.println("1212");
         System.out.println("测试");
+        System.out.println("重新配置测试");
     }
 }
